8518820e4e7488e1fe2b59490e3f28eac9e0850f
itaibn9 <itaibn9@gmail.com>
2020-10-06 12:37:11 +0300

    full solution

--- src/Components/SideBarItem/SideBarItem.js
+++ src/Components/SideBarItem/SideBarItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./sideBarItem.css";
 import {useRecoilState, useSetRecoilState} from 'recoil';
 import {rectangleIdArrayState} from '../atoms';
@@ -12,11 +12,11 @@ function SideBarItem({id}) {
 	const [heightInput, setHeightInput] = useState(rectAtom.height);
 	const [colorInput, setColorInput] = useState(rectAtom.color);
 
+useEffect(() => {
+onChange()
+}, [widthInput, heightInput, colorInput])
+
 	const onChange = () => {
-		// setRectanglesArray((oldArray) =>
-		//  [...oldArray.slice(0, index),
-		// 	 {width:widthInput, height:heightInput, color:colorInput},
-		// 	  ...oldArray.slice(index + 1)]);
 		setRectAtom( {...rectAtom, width: parseInt(widthInput), height:parseInt(heightInput), color:colorInput })
 	}
 	return (
@@ -26,19 +26,16 @@ function SideBarItem({id}) {
 			<input id="shapeWidthInput" type="number" placeholder={rectAtom.width} min="0" value={widthInput}
 			onChange={({ target }) => {
 				 setWidthInput(target.value);
-				 onChange();
 			}} />
 			<label>Height: </label>
 			<input id="shapeHeightInput" type="number" placeholder={rectAtom.height} min="0" value={heightInput}
 			onChange={({ target }) => {
 				setHeightInput(target.value);
-				onChange();
 				}}/>
 			<label>Color :</label>
 			<input type="color" id="colorModifier" value={colorInput} 
 			onChange={({ target }) => {
 				 setColorInput(target.value);
-				 onChange();
 			}}/>
 		</li>
 	);
